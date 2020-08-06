1150fbd9b0cd2d9430a4e8d029c1d67d374eeafb
hafizeakyildiz <hafizeakyildiz1@gmail.com>
2020-08-05 20:31:06 -0400

    add ReverseAString.java

--- src/utility/ZombieAttack.java
+++ src/utility/ZombieAttack.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class ZombieAttack {
     public static void main(String[] args) {
 
+        // I am adding these line to see in git
+
                 Scanner input = new Scanner(System.in);
                 int[] inhabitants = new int[8];
                 for(int i=0; i<inhabitants.length; i++) {
